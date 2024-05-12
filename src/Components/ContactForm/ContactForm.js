8701a5f5e46e9bf84f21631d4d644f4d3f2250cf
Mukund Mohan <mukundmohan22@gmail.com>
2024-05-12 22:07:02 +0530

    Added About page and contact me page

--- src/Components/ContactForm/ContactForm.js
+++ src/Components/ContactForm/ContactForm.js
@@ -3,24 +3,31 @@ import React from "react";
 import "./ContactForm.scss";
 
 const ContactForm = () => {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    console.log("formData", JSON.stringify(formData));
+  };
   return (
-    <div className="contact-form">
-      <div className="contact-form--row">
-        <input type="text" placeholder="Name" />
+    <form onSubmit={handleSubmit}>
+      <div className="contact-form">
+        <div className="contact-form--row">
+          <input type="text" placeholder="Name" name="name" required />
+        </div>
+        <div className="contact-form--row">
+          <input type="text" placeholder="Email Address" name="email" required />
+        </div>
+        <div className="contact-form--row">
+          <textarea type="text" placeholder="Your Message" rows={2} name="message" required />
+        </div>
+        <div className="contact-form--row">
+          <button type="submit">
+            <i class="fa-regular fa-paper-plane"></i>
+            Submit
+          </button>
+        </div>
       </div>
-      <div className="contact-form--row">
-        <input type="text" placeholder="Email Address" />
-      </div>
-      <div className="contact-form--row">
-        <input type="text" placeholder="Your Message" aria-rowspan={5} aria-colspan={50} />
-      </div>
-      <div className="contact-form--row">
-        <button type="submit">
-          <i class="fa-regular fa-paper-plane"></i>
-          Submit
-        </button>
-      </div>
-    </div>
+    </form>
   );
 };
 
